4765ed8e1dcd2fc3e718c8610bb6df408eab33d3
Theresa Deering <trisapeace@Theresas-Mini.hitronhub.home>
2020-11-30 10:07:09 -0800

    Add Storybook
    - Right now this only shows the default stories that comes when
      storybook is initialized.
    - For some reason, the emulator isn't connecting to the browser, so the
      navigation through the browser isn't working, but this isn't as
      important as the navigation through the emulator, so I'll leave this for
      now. Richard is planning to look into this more for BeSafe at some point
      and hopefully we can fix it here too.

---
+++ src/Root.jsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useState, useEffect } from "react"
+import StorybookUI from "../storybook"
+import App from "./App"
+
+function Root() {
+    const [storybookActive, setStorybookActive] = useState(false)
+
+    const toggleStorybook = useCallback(
+        () => setStorybookActive((active) => !active),
+        []
+    )
+
+    useEffect(() => {
+        // eslint-disable-next-line no-undef
+        if (__DEV__) {
+            // eslint-disable-next-line global-require
+            const DevMenu = require("react-native-dev-menu")
+            DevMenu.addItem("Toggle Storybook", toggleStorybook)
+        }
+    }, [toggleStorybook])
+
+    return storybookActive ? <StorybookUI /> : <App />
+}
+
+export default Root
