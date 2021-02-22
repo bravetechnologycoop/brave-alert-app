28f3c6e94b525aa4287414dfb9c0d55a3e141b6a
Theresa Deering <trisapeace@Theresas-Mini.hitronhub.home>
2021-02-22 11:33:02 -0800

    Add FontAwesome icons to tab navigation bar

--- src/navigation/MainTabs.jsx
+++ src/navigation/MainTabs.jsx
@@ -1,6 +1,8 @@
 // Third-party dependencies
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHomeAlt, faExclamationCircle, faCheckSquare, faCommentAlt } from '@fortawesome/pro-light-svg-icons'
 
 // In-house dependencies
 import ExampleScreen from '../screens/ExampleScreen'
@@ -14,9 +16,43 @@ function MainTabs() {
     <Tab.Navigator
       headerMode="screen" // See https://reactnavigation.org/docs/stack-navigator#set-headermode-to-screen
       initialRouteName={SCREEN.EXAMPLE}
+      tabBarOptions={{
+        activeTintColor: '#00857A',
+        inactiveTintColor: '#000000',
+      }}
     >
-      <Tab.Screen name={SCREEN.EXAMPLE} component={ExampleScreen} />
-      <Tab.Screen name={SCREEN.EXAMPLE2} component={ExampleScreen2} />
+      <Tab.Screen
+        name={SCREEN.EXAMPLE}
+        component={ExampleScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faHomeAlt} size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name={SCREEN.EXAMPLE2}
+        component={ExampleScreen2}
+        options={{
+          tabBarLabel: 'Alert History',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faExclamationCircle} size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name={SCREEN.NOTIFICATIONS}
+        component={ExampleScreen2}
+        options={{
+          tabBarLabel: 'Notifications',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faCheckSquare} size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name={SCREEN.CONTACT}
+        component={ExampleScreen2}
+        options={{
+          tabBarLabel: 'Contact',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faCommentAlt} size={size} color={color} />,
+        }}
+      />
     </Tab.Navigator>
   )
 }
--- src/navigation/ScreensEnum.js
+++ src/navigation/ScreensEnum.js
@@ -1,8 +1,10 @@
 const SCREEN = {
+  CONTACT: 'ContactScreen',
   ERROR: 'ErrorScreen',
   EXAMPLE: 'ExampleScreen',
   EXAMPLE2: 'ExampleScreen2',
   MAIN: 'MainScreen',
+  NOTIFICATIONS: 'NotificationsScreen',
 }
 
 export default SCREEN
